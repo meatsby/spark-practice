350b8449040de91bce9c670f3c42c884cf4b7f8e
meatsby <meatsby@gmail.com>
2022-04-01 16:37:46 +0900

    feat: practice html and post

---
+++ README.md
@@ -0,0 +1,10 @@
+## 1. getting started with spark.java
+
+- [x] create gradle project
+- [x] add spark.java dependency
+- [x] practice get method - request and response data
+
+## 2. html and post
+
+- [x] create html file and UI
+- [x] practice post method - deliver data to server
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,12 @@
 import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
+        staticFiles.location("/static");
+
         // localhost:4567/hello 요청
         // "Hello World" 응답
         // get("/hello", (req, res) -> "Hello World");
@@ -13,6 +17,6 @@ public class HelloWorld {
 
         // localhost:4567/hello?name={name} 요청
         // "Hello {name} 응답
-        get("/hello", (req, res) -> "Hello " + req.queryParams("name") + " 나이는 " + req.queryParams("age"));
+        post("/hello", (req, res) -> "Hello " + req.queryParams("name") + " 나이는 " + req.queryParams("age"));
     }
 }
