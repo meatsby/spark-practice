6ef743a74d38c28c34438fa028219b26cfe1fdd1
meatsby <meatsby@gmail.com>
2022-04-01 16:03:02 +0900

    feat: practice get

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,16 @@ import static spark.Spark.get;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        // localhost:4567/hello 요청
+        // "Hello World" 응답
+        // get("/hello", (req, res) -> "Hello World");
+
+        // localhost:4567/hello/{:name} 요청
+        // "Hello {:name}" 응답
+        // get("/hello/:name", (req, res) -> "Hello " + req.params(":name"));
+
+        // localhost:4567/hello?name={name} 요청
+        // "Hello {name} 응답
+        get("/hello", (req, res) -> "Hello " + req.queryParams("name") + " 나이는 " + req.queryParams("age"));
     }
 }
