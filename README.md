66be411403c5f41358c147025293345d89dad333
meatsby <meatsby@gmail.com>
2022-04-01 17:24:36 +0900

    feat: practice handlebars template engine

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 
 - [x] create html file and UI
 - [x] practice post method - deliver data to server
+
+## 3. template engine
+
+- [x] what is template engine?
+  - html 파일을 동적으로 만들 때 사용
+  - render 메서드를 통해 model 과 template 이 될 html 파일을 응답
+    - model 은 `Map<String, Object>` 형식으로 구성되어 있음
+    - model.put("동적 html 파일에 사용 될 변수명", req.queryParams("정적 html 파일로 부터 받은 변수명"))
+    - 동적 html 파일에 사용 될 변수는 `{{변수명}}` 형식으로 동적 html 파일에 추가되어야 함
+  - 동적 html 파일은 `templates` 디렉터리에 존재해야 함
+- [x] add handlebars template engine dependency
+- [x] deliver data to handlebars template engine
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,11 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.staticFiles;
 
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
@@ -17,6 +22,18 @@ public class HelloWorld {
 
         // localhost:4567/hello?name={name} 요청
         // "Hello {name} 응답
-        post("/hello", (req, res) -> "Hello " + req.queryParams("name") + " 나이는 " + req.queryParams("age"));
+        get("/hello", (req, res) -> "Hello " + req.queryParams("name") + " 나이는 " + req.queryParams("age"));
+
+        post("/hello", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("name", req.queryParams("name"));
+            model.put("age", req.queryParams("age"));
+
+            return render(model, "result.html");
+        });
+    }
+
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
